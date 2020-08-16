12bd4fb072b111bdd4fcfc270a7f8a3ece767108
Makoto Onoue <monoue@student.42tokyo.jp>
2020-08-17 08:09:17 +0900

    split fixed

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: monoue <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/26 17:04:52 by monoue            #+#    #+#             */
-/*   Updated: 2020/07/09 13:37:31 by monoue           ###   ########.fr       */
+/*   Updated: 2020/08/17 08:07:59 by monoue           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,16 +30,20 @@ static int		ft_count_words(char *str, char c)
 	return (count);
 }
 
-static size_t	set_start(size_t start, size_t end, char const *s, char c)
+static size_t	set_start(size_t end, char const *s, char c)
 {
+	size_t	start;
+
 	start = end;
 	while (start < ft_strlen(s) && s[start] == c && s[start] != '\0')
 		start++;
 	return (start);
 }
 
-static size_t	set_end(size_t start, size_t end, char const *s, char c)
+static size_t	set_end(size_t start, char const *s, char c)
 {
+	size_t	end;
+
 	end = start + 1;
 	while (s[end] != '\0' && s[end] != c)
 		end++;
@@ -56,7 +60,7 @@ static void		free_all(char **arr, size_t i)
 	free(arr);
 }
 
-char			**ft_split(char const *s, char c)
+char			**ft_split(char const *str, char sep_c)
 {
 	char	**arr;
 	size_t	count;
@@ -64,23 +68,23 @@ char			**ft_split(char const *s, char c)
 	size_t	end;
 	size_t	i;
 
-	if (s == NULL)
+	if (str == NULL)
 		return (NULL);
-	count = ft_count_words((char *)s, c);
+	count = ft_count_words((char *)str, sep_c);
 	if (!(arr = ft_calloc((count + 1), sizeof(*arr))))
 		return (NULL);
-	if (c == '\0')
+	if (sep_c == '\0')
 		return (arr);
 	i = 0;
-	start = set_start(0, 0, s, c);
-	while (count-- > 0 && (end = set_end(start, end, s, c)))
+	start = set_start(0, str, sep_c);
+	while (count-- > 0 && (end = set_end(start, str, sep_c)))
 	{
-		if (!(arr[i++] = ft_substr(s, start, end - start)))
+		if (!(arr[i++] = ft_substr(str, start, end - start)))
 		{
-			free_all(arr, --i);
+			free_all(arr, i - 1);
 			return (NULL);
 		}
-		start = set_start(start, end, s, c);
+		start = set_start(end, str, sep_c);
 	}
 	return (arr);
 }
