44c10c7cd032d6314655d672c9786f463107b04e
Makoto Onoue <monoue@student.42tokyo.jp>
2020-08-26 09:44:08 +0900

    fixed

--- ft_substr.c
+++ ft_substr.c
@@ -6,17 +6,17 @@
 /*   By: monoue <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/26 13:48:12 by monoue            #+#    #+#             */
-/*   Updated: 2020/08/26 09:37:55 by monoue           ###   ########.fr       */
+/*   Updated: 2020/08/26 09:43:49 by monoue           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_substr(char const *original_s, unsigned int start, size_t cut_len)
+char	*ft_substr(char const *original_s, unsigned int start, size_t len)
 {
 	char	*sub_s;
 	size_t	srclen;
-	size_t	minlen;
+	size_t	cut_len;
 	size_t	sub_index;
 	size_t	original_index;
 
@@ -25,16 +25,16 @@ char	*ft_substr(char const *original_s, unsigned int start, size_t cut_len)
 	srclen = ft_strlen(original_s);
 	if (start > srclen)
 		return (ft_strdup(""));
-	minlen = MIN(srclen - start, len);
-	sub_s = malloc(sizeof(*sub_s) * (minlen + 1));
+	cut_len = MIN(srclen - start, len);
+	sub_s = malloc(sizeof(*sub_s) * (cut_len + 1));
 	if (sub_s == NULL)
 		return (NULL);
 	sub_index = 0;
 	original_index = start;
-	while (minlen > 0)
+	while (cut_len > 0)
 	{
 		sub_s[sub_index++] = original_s[original_index++];
-		minlen--;
+		cut_len--;
 	}
 	sub_s[sub_index] = '\0';
 	return (sub_s);
