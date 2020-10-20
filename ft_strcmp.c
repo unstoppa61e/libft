b24bc50b9d319e94452cd7f6d58808395fb9f476
Makoto Onoue <monoue@c1r1s5.42tokyo.jp>
2020-10-20 14:02:14 +0900

    strcmp simplified

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: monoue <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: monoue <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/06/22 13:50:34 by monoue            #+#    #+#             */
-/*   Updated: 2020/10/14 15:42:30 by monoue           ###   ########.fr       */
+/*   Created: 2020/10/20 13:56:53 by monoue            #+#    #+#             */
+/*   Updated: 2020/10/20 14:00:32 by monoue           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,9 @@
 int		ft_strcmp(const char *s1, const char *s2)
 {
 	size_t			index;
-	const size_t	s1_len = ft_strlen(s1);
-	const size_t	s2_len = ft_strlen(s2);
-	const size_t	min_str_len = MIN(s1_len, s2_len);
 
 	index = 0;
-	while (index < min_str_len)
-	{
-		if (s1[index] != s2[index])
-			return ((int)((unsigned char)s1[index] - (unsigned char)s2[index]));
+	while (s1[index] != '\0' && s2[index] != '\0' && s1[index] == s2[index])
 		index++;
-	}
-	if (s1_len > s2_len)
-		return (1);
-	if (s1_len < s2_len)
-		return (-1);
-	return (0);
+	return (s1[index] - s2[index]);
 }
