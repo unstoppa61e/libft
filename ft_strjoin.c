973f0274842c2de966d4976a3bcb5cee0bc9358e
Makoto Onoue <monoue@c1r1s5.42tokyo.jp>
2021-01-20 14:31:35 +0900

    strjoin perfected

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: monoue <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/26 16:01:19 by monoue            #+#    #+#             */
-/*   Updated: 2021/01/20 13:53:34 by monoue           ###   ########.fr       */
+/*   Updated: 2021/01/20 14:31:15 by monoue           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,14 @@ char	*ft_strjoin(char const *s1, char const *s2)
 {
 	const size_t	s1_len = ft_strlen(s1);
 	const size_t	s2_len = ft_strlen(s2);
-	char			*joined_s;
+	char			*ret_s;
 
 	if (!s1 || !s2)
 		return (NULL);
-	joined_s = malloc(sizeof(char) * (s1_len + s2_len + 1));
-	if (!joined_s)
+	ret_s = malloc(sizeof(char) * (s1_len + s2_len + 1));
+	if (!ret_s)
 		return (NULL);
-	ft_strcpy(joined_s, s1);
-	ft_strcpy(&joined_s[s1_len], s2);
-	return (joined_s);
+	ft_strcpy(ret_s, s1);
+	ft_strcpy(&ret_s[s1_len], s2);
+	return (ret_s);
 }
