01f815cafe113eef028405e535d198783d1b9428
Makoto Onoue <monoue@c1r1s5.42tokyo.jp>
2021-02-10 08:54:32 +0900

    strnjoin fixed

--- ft_strnjoin.c
+++ ft_strnjoin.c
@@ -6,7 +6,7 @@
 /*   By: monoue <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/26 16:01:19 by monoue            #+#    #+#             */
-/*   Updated: 2021/02/10 08:28:45 by monoue           ###   ########.fr       */
+/*   Updated: 2021/02/10 08:53:47 by monoue           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 char	*ft_strnjoin(char const *s1, char const *s2, size_t n)
 {
 	const size_t	s1_len = ft_strlen(s1);
-	const size_t	join_len = ft_strnlen(s2, n);
+	const size_t	s2_nlen = ft_strnlen(s2, n);
 	char			*ret_s;
 
 	if (!s1 && !s2)
@@ -24,10 +24,10 @@ char	*ft_strnjoin(char const *s1, char const *s2, size_t n)
 		return (ft_strndup(s2, n));
 	if (!s2)
 		return (ft_strdup(s1));
-	ret_s = malloc((s1_len + join_len + 1) * sizeof(char));
+	ret_s = malloc((s1_len + s2_nlen + 1) * sizeof(char));
 	if (!ret_s)
 		return (NULL);
 	ft_strcpy(ret_s, s1);
-	ft_strncpy(&ret_s[s1_len], s2, join_len);
+	ft_strncpy(&ret_s[s1_len], s2, s2_nlen);
 	return (ret_s);
 }
